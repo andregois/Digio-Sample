ef99139faff9737a5bb8a537678f056b71aaa14c
André Yuri <andre.gois@virtus.ufcg.edu.br>
2020-11-27 10:37:45 -0300

    ajusting tests

--- app/src/test/java/com/andregois/digioapp/data/MainViewModelTest.kt
+++ app/src/test/java/com/andregois/digioapp/data/MainViewModelTest.kt
@@ -6,8 +6,6 @@ import com.andregois.digioapp.domain.Cash
 import com.andregois.digioapp.domain.DigioResponse
 import com.andregois.digioapp.domain.Product
 import com.andregois.digioapp.domain.Spotlight
-import com.andregois.digioapp.retrofit.DigioApi
-import com.andregois.digioapp.utils.ApiResult
 import com.andregois.digioapp.utils.Status
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
@@ -17,15 +15,12 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
-import net.bytebuddy.implementation.bytecode.Throw
 import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.MockitoAnnotations
-import java.io.IOException
-import java.lang.Exception
 
 
 @ExperimentalCoroutinesApi
@@ -38,7 +33,6 @@ class MainViewModelTest {
 
     lateinit var result: DigioResponse
 
-
     lateinit var digioDataSource: DigioDataSource
     lateinit var digioRepository: DigioRepository
 
@@ -46,18 +40,11 @@ class MainViewModelTest {
 
     @Before
     fun setup() = runBlocking {
-//        digioApi = mock()
-        result = DigioResponse(
-            Cash("https://s3-sa-east-1.amazonaws.com/digio-exame/cash_banner.png", "Dinheiro na conta sem complicação. Transfira parte do limite do seu cartão para sua conta.", "digio Cash"),
-            listOf(Product("Com o e-Gift Card Level Up você adquire créditos para jogar diversas opções de games, de MMORPGs a jogos de Tiro e Mobas! Para ver a lista de jogos e a quantia de créditos que pode resgatar em cada um deles, acesse: http://levelupgames.uol.com.br/levelup/seuegiftcard.lhtml",  "https://s3-sa-east-1.amazonaws.com/digio-exame/level_up_icon.png", "Level up")),
-            listOf(Spotlight("https://s3-sa-east-1.amazonaws.com/digio-exame/recharge_banner.png", "Agora ficou mais fácil colocar créditos no seu celular! A digio Store traz a facilidade de fazer recargas... direto pelo seu aplicativo, com toda segurança e praticidade que você procura.", "Recarga"))
-        )
-
-        digioDataSource = mock()
-
-
-        digioRepository = DigioRepositoryImpl(digioDataSource)
         MockitoAnnotations.initMocks(this)
+
+        result = createDigioResponse()
+        digioDataSource = mock()
+        digioRepository = DigioRepositoryImpl(digioDataSource)
         Dispatchers.setMain(testDispatcher)
     }
 
@@ -70,26 +57,26 @@ class MainViewModelTest {
     @Test
     fun `when view model fetchData get success then sets response`() = runBlocking {
         whenever(digioDataSource.getDigioData()).thenReturn(result)
+
         viewModel = MainViewModel(digioRepository)
 
-        viewModel.fetchData()
-
         viewModel.response.observeOnce() {
+            assertEquals(Status.SUCCESS, it.status)
             assertEquals(result, it.data)
+
         }
     }
 
 
     @Test
     fun `when view model fetchData get error then sets response`() = runBlocking {
-        whenever(digioDataSource.getDigioData()).thenThrow(ArrayIndexOutOfBoundsException(""))
-        viewModel = MainViewModel(digioRepository)
+        whenever(digioDataSource.getDigioData()).thenThrow(RuntimeException(""))
 
-        viewModel.fetchData()
+        viewModel = MainViewModel(digioRepository)
 
         viewModel.response.observeOnce() {
             assertEquals(null, it.data)
-            assertEquals(Status.ERROR,it.status)
+            assertEquals(Status.ERROR, it.status)
         }
     }
 
@@ -98,4 +85,28 @@ class MainViewModelTest {
 fun <T> LiveData<T>.observeOnce(onChangeHandler: (T) -> Unit) {
     val observer = OneTimeObserver(handler = onChangeHandler)
     observe(observer, observer)
+}
+
+private fun createDigioResponse(): DigioResponse {
+    return DigioResponse(
+        Cash(
+            "https://s3-sa-east-1.amazonaws.com/digio-exame/cash_banner.png",
+            "Dinheiro na conta sem complicação. Transfira parte do limite do seu cartão para sua conta.",
+            "digio Cash"
+        ),
+        listOf(
+            Product(
+                "Com o e-Gift Card Level Up você adquire créditos para jogar diversas opções de games, de MMORPGs a jogos de Tiro e Mobas! Para ver a lista de jogos e a quantia de créditos que pode resgatar em cada um deles, acesse: http://levelupgames.uol.com.br/levelup/seuegiftcard.lhtml",
+                "https://s3-sa-east-1.amazonaws.com/digio-exame/level_up_icon.png",
+                "Level up"
+            )
+        ),
+        listOf(
+            Spotlight(
+                "https://s3-sa-east-1.amazonaws.com/digio-exame/recharge_banner.png",
+                "Agora ficou mais fácil colocar créditos no seu celular! A digio Store traz a facilidade de fazer recargas... direto pelo seu aplicativo, com toda segurança e praticidade que você procura.",
+                "Recarga"
+            )
+        )
+    )
 }
